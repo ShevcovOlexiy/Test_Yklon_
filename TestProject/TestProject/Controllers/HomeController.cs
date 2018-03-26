9c1e2a3d5835554b7106dfe8de684f01d973ce49
Olexii <olexiiforwork@gmail.com>
2018-03-27 00:18:16 +0300

    1

---
+++ TestProject/TestProject/Controllers/HomeController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TestProject.Models;
+using System.Xml;
+
+namespace TestProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<ClassCountry> Countrys = ClassCountry.DefaultListCountry();
+        static Hashtable listH = ClassToun.CreateListToun();
+        static List<ClassToun> Toun = ClassToun.DefaultListToun(listH, "225");
+
+        static int GetRecvest( string regionCode, string timeStamp,ref string timeStr, ref string ColorStr, ref string hintStr, ref string LevelStr, ref string titleStr)
+        {
+            var content = new MemoryStream();
+            string url =
+               String.Format("https://export.yandex.com/bar/reginfo.xml?region={0}&bustCache={1}", regionCode, timeStamp);
+
+
+               // String.Format("http://hidedoor.com/servlet/redirect.srv/szx/sfnnyml/sihfhqy/p2/bar/reginfo.xml?region={0}&bustCache={1}", regionCode, timeStamp);
+            var WebReq = (HttpWebRequest)WebRequest.Create(url);
+            using (var resp = WebReq.GetResponse())
+                using (var str=resp.GetResponseStream())
+                {
+
+                if (str != null)
+                {
+                    str.CopyTo(content);
+                }
+                else return 0;
+
+                    content.Position = 0;
+                    XmlDocument xmlDoc = new XmlDocument();
+                try
+                {
+
+                    xmlDoc.Load(content);
+                    string path = System.IO.Directory.GetCurrentDirectory() + "\\" + "File.xml";
+
+                    xmlDoc.Save(path);
+                    XmlNode node = xmlDoc.DocumentElement;
+                    if (node.Name == "info")
+                    {
+                        XmlAttributeCollection at = node.Attributes;
+                        string lang = at.GetNamedItem("lang").Value;
+
+
+
+                        XmlNodeList list = node.ChildNodes;
+                        foreach (XmlNode s in list)
+                        {
+                            if (s.Name == "region")
+                            {
+                                foreach (XmlNode stitle in s.ChildNodes)
+                                {
+                                    if (stitle.Name == "title")
+                                    { titleStr  = stitle.InnerText; }
+                                }
+                            }
+
+                            if (s.Name == "traffic")
+                            {
+                                XmlAttributeCollection a_tr = s.Attributes;
+                                //richTextBox1.AppendText("region=\"" + a_tr.GetNamedItem("region").Value + "\"\n");
+
+                                foreach (XmlNode s1 in s.ChildNodes)
+                                {
+                                    if (s1.Name == "region")
+                                    {
+                                        XmlAttributeCollection a_reg = s1.Attributes;
+                                        if (a_reg.GetNamedItem("id").Value != regionCode)
+                                            return content.ToArray().Length;
+
+                                        foreach (XmlNode s2 in s1.ChildNodes)
+                                        {
+
+                                            if (s2.Name == "level")
+                                            {
+
+                                                LevelStr = s2.InnerText;
+                                            }
+
+                                            if (s2.Name == "icon")
+                                            {
+                                                ColorStr = s2.InnerText;
+                                            }
+
+                                            if (s2.Name == "hint")
+                                            {
+                                                if (s2.Attributes.GetNamedItem("lang").Value == lang)
+                                                    hintStr = s2.InnerText;
+                                            }
+
+                                            if (s2.Name == "time")
+                                            {
+                                                timeStr = s2.InnerText;
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (XmlException e)
+                { return -1; }
+
+            }
+
+            return content.ToArray().Length;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ClassToun.CreateListToun();
+            SelectList list = new SelectList(Countrys,"Code", "Country", Countrys[0].Code);
+
+            SelectList list1 = new SelectList(Toun, "Code", "Toun", Toun[1].Code);
+            string ss=list1.SelectedValue.ToString();
+            string ts= "5";
+            string timeStr = "", ColorStr = "", hintStr = "", LevelStr = "", titleStr="";
+            int res=GetRecvest( ss, ts,ref timeStr , ref ColorStr,ref hintStr, ref LevelStr, ref titleStr);
+
+            ViewBag.CountryList = list;
+            ViewBag.TounList = list1;
+
+            ViewBag.Time = timeStr;
+            ViewBag.Level = LevelStr;
+            ViewBag.color = ColorStr;
+            ViewBag.Hint = hintStr;
+            ViewBag.TitleToun = titleStr;
+            //if(res>0)
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Select1, string Select2)
+        {
+            ClassToun.CreateListToun();
+            SelectList list = new SelectList(Countrys, "Code", "Country", Select1);
+            Toun = ClassToun.DefaultListToun(listH, Select1);
+            bool ret = false;
+            foreach(ClassToun buff in Toun)
+            {
+                if (buff.Code == Select2)
+                    ret = true;
+
+            }
+            SelectList list1 = new SelectList(Toun, "Code", "Toun", (!ret) ? Toun[0].Code : Select2);
+        string ss = list1.SelectedValue.ToString();
+        string ts = "5";
+            string timeStr = "", ColorStr = "", hintStr = "", LevelStr = "", titleStr = "";
+            int res = GetRecvest(ss, ts, ref timeStr, ref ColorStr, ref hintStr, ref LevelStr, ref titleStr);
+
+            ViewBag.CountryList = list;
+            ViewBag.TounList = list1;
+
+            ViewBag.Time = timeStr;
+            ViewBag.Level = LevelStr;
+            ViewBag.color = ColorStr;
+            ViewBag.Hint = hintStr;
+            ViewBag.TitleToun = titleStr;
+            if (res==-1)
+            ViewBag.Error = "Данные какимто образом не коректны"; 
+            return View();
+        }
+
+    }
+}
---
+++ TestProject/TestProject/Models/ClassCantry.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Models
+{
+    public class ClassCountry
+    {
+        public string Code { get; set; }
+        public string Country { get; set; }
+        static public List<ClassCountry> DefaultListCountry()
+        { return new List<ClassCountry>()
+           {
+            new ClassCountry(){ Code="225", Country ="Россия" },
+            new ClassCountry() { Code = "7", Country = "Америка" },
+            new ClassCountry() { Code = "120", Country = "Белорусь" },
+            new ClassCountry() { Code = "149", Country = "Украина" }
+            };
+        }
+
+    }
+
+    public class ClassToun
+    {
+        public string Code { get; set; }
+        public string Toun { get; set; }
+
+
+        
+
+        static public Hashtable CreateListToun()
+        {
+
+            Hashtable list = new Hashtable();
+
+                list.Add("225", new List<ClassToun>() {
+            new ClassToun(){ Code="213", Toun ="Москва" },
+            new ClassToun() { Code = "2", Toun = "Санкт-Петербург" },
+            new ClassToun() { Code = "10765", Toun = "Щелково" },
+            new ClassToun() { Code = "21622", Toun = "Железнодорожный" },
+            new ClassToun() { Code = "10725", Toun = "Домодедово" },
+            new ClassToun() { Code = "11015", Toun = "Республика Калмыкия" }
+            });
+                list.Add("7", new List<ClassToun>() {
+            new ClassToun(){ Code="202", Toun ="Нью-Йорк" },
+            new ClassToun() { Code = "223", Toun = "Бостон" },
+            new ClassToun() { Code = "86", Toun = "Атланта" }
+            });
+                list.Add("120", new List<ClassToun>() {
+            new ClassToun(){ Code="157", Toun ="Минск" },
+            new ClassToun() { Code = "155", Toun = "Гомель" }
+            });
+                list.Add("149", new List<ClassToun>() {
+            new ClassToun(){ Code="145", Toun ="Одесса" },
+            new ClassToun() { Code = "960", Toun = "Запорожье" },
+            new ClassToun() { Code = "10367", Toun = "Мелитополь" },
+            new ClassToun() { Code = "143", Toun = "Киев" }
+            
+            });
+
+            return list;
+            
+        }
+        static public List<ClassToun> DefaultListToun(Hashtable list,string Key)
+        {
+            if (list == null)
+                return new List<ClassToun>() { new ClassToun() { Code = "-", Toun = "Города отсутствуют" } };
+
+            if (list.ContainsKey(Key))
+                return ((List < ClassToun >)list[Key]);
+            else
+                return new List<ClassToun>() { new ClassToun() { Code = "-", Toun = "Города отсутствуют" } };
+        }
+
+    }
+
+} 
